02223facd9d9f8b0dc4b5e30901afa3775c515a2
NakulanT <nakult721@gmail.com>
2023-11-09 21:14:07 +0530

    Added Search Button

--- frontend/src/pages/Products/index.js
+++ frontend/src/pages/Products/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Cards from "../../components/Card";
-import { Grid, Box, Flex, Button } from "@chakra-ui/react";
+import { Grid, Box, Flex, Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { useInfiniteQuery } from "react-query";
 import { fetchProductList } from "../../api.js";
 
 function Products() {
+  const [searchTerm, setSearchTerm] = useState("");
+  
   const {
     data,
     error,
@@ -24,25 +26,39 @@ function Products() {
     },
   });
 
+  const filteredData = data?.pages.flatMap((group) => group).filter((item) =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   if (status === "loading") return "Loading...";
 
   if (status === "error") return "An error has occurred: " + error.message;
 
   return (
     <div>
+      <InputGroup mb={4} mr={4}>
+        <Input
+          placeholder="Search products by title..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={() => setSearchTerm("")}>
+            Clear
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      
       <div className="products">
         <Grid templateColumns="repeat(3,1fr)" gap={4}>
-          {data.pages.map((group, i) => (
-            <React.Fragment key={i}>
-              {group.map((item) => (
-                <Box w="100%" key={item._id}>
-                  <Cards item={item} />
-                </Box>
-              ))}
-            </React.Fragment>
+          {filteredData.map((item) => (
+            <Box w="100%" key={item._id}>
+              <Cards item={item} />
+            </Box>
           ))}
         </Grid>
       </div>
+      
       <Flex mt="10" justifyContent="center">
         <Button
           onClick={() => fetchNextPage()}
@@ -61,3 +77,4 @@ function Products() {
 }
 
 export default Products;
+
